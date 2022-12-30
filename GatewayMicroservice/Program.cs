866acf7ff230421b349e58d9aa29922a2b36d801
tavd-projects <97411141+tavd-projects@users.noreply.github.com>
2022-12-30 16:58:09 +0300

    Initial commit

---
+++ GatewayMicroservice/Program.cs
@@ -0,0 +1,35 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Requester;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add ocelot
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await app.UseOcelot();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
